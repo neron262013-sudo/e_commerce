bc3e5f044f919a12239950e2dcd7fb55fb603910
Роман <nero.n262013@gmail.com>
2026-03-18 01:40:41 +0300

    classes.py update task 14_2

--- src/classes.py
+++ src/classes.py
@@ -1,26 +1,61 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        else:
+            self.__price = price
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(product_data["name"],
+                   product_data["description"],
+                   product_data["price"],
+                   product_data["quantity"])
 
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += (f"{product.name}, {product.price} руб, Остаток: {product.quantity} шт.\n")
+        return products_str
+
+    @property
+    def product_list(self):
+        return self.__products
+
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -15,12 +15,12 @@ def test_category_counters(first_category, second_category):
     assert first_category.name == "Смартфоны"
     assert first_category.description == ("Смартфоны, как средство не только коммуникации, "
                                           "но и получения дополнительных функций для удобства жизни")
-    assert len(first_category.products) == 3
+    assert len(first_category.product_list) == 3
 
     assert second_category.name == "Телевизоры"
     assert second_category.description == ("Современный телевизор, который позволяет наслаждаться просмотром, "
                                            "станет вашим другом и помощником")
-    assert len(second_category.products) == 1
+    assert len(second_category.product_list) == 1
 
     assert first_category.category_count == 2
     assert first_category.product_count == 4
