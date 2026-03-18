df50ee5ec1a8927b05fed49f8546e91ea0a44c10
Роман <nero.n262013@gmail.com>
2026-03-18 03:08:57 +0300

    tests update

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,20 @@ def first_product():
                    price=180000.0,
                    quantity=5)
 
+@pytest.fixture()
+def first_product_dict():
+    return {"name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5}
+
+@pytest.fixture()
+def second_product():
+    return Product(name="Iphone 15",
+                   description="512GB, Gray space",
+                   price=210000.0,
+                   quantity=8)
+
 
 @pytest.fixture()
 def first_category():
