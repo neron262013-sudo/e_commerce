8a776a72a10149977af36955128af1748a622a2a
Роман <nero.n262013@gmail.com>
2026-03-25 16:18:55 +0300

    MixInfo fix and readme update

--- src/classes.py
+++ src/classes.py
@@ -33,7 +33,7 @@ class MixinInfo:
         print(repr(self))
 
     def __repr__(self):
-        return f"{self.__class__.__name__}, {self.description}, {self.price}, {self.quantity}"
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
 
 
 class Product(BaseProduct, MixinInfo):
