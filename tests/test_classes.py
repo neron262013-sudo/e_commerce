90493e30ac8df6467a9155e6f3984be9c634637f
Роман <nero.n262013@gmail.com>
2026-03-25 16:03:12 +0300

    tests

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.classes import Product
+from src.classes import Product, Smartphone, LawnGrass
 
 
 def test_product_init(first_product):
@@ -134,3 +134,34 @@ def test_lawn_grass_add_exception(first_lawn_grass, first_smartphone):
         first_lawn_grass + 1
     with pytest.raises(TypeError):
         first_lawn_grass + first_smartphone
+
+
+
+def test_mixin_info_print(capsys):
+    Product(name="Samsung Galaxy S23 Ultra",
+            description="256GB, Серый цвет, 200MP камера",
+            price=180000.0,
+            quantity=5)
+    message = capsys.readouterr()
+    assert message.out.strip() == "Product, 256GB, Серый цвет, 200MP камера, 180000.0, 5"
+
+    Smartphone("Samsung Galaxy S23 Ultra",
+               "256GB, Серый цвет, 200MP камера",
+               180000.0,
+               5,
+               95.5,
+               "S23 Ultra",
+               256,
+               "Серый")
+    message = capsys.readouterr()
+    assert message.out.strip() == "Smartphone, 256GB, Серый цвет, 200MP камера, 180000.0, 5"
+
+    LawnGrass("Газонная трава",
+              "Элитная трава для газона",
+              500.0,
+              20,
+              "Россия",
+              "7 дней",
+              "Зеленый")
+    message = capsys.readouterr()
+    assert message.out.strip() == "LawnGrass, Элитная трава для газона, 500.0, 20"
